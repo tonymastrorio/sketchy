1eccc4b69d86b736de935da08c988b4dc45a7986
Tony Mastrorio <tony.mastrorio@gmail.com>
2017-03-14 13:08:24 -0400

    added comments

--- sketchy.js
+++ sketchy.js
@@ -1,4 +1,5 @@
-var boxRow = 20;
+var boxRow = prompt("How many pixels per side?");
+
 var totalBoxes = boxRow * boxRow;
 var boxWidth = 800 / boxRow;
 var boxHeight = boxWidth;
@@ -6,10 +7,11 @@ var boxHeight = boxWidth;
 var backgroundCol = "red";
 var mouseCol = "orange";
 
+// add selected class on page load
 $("#bgcolors > .red").addClass("selected");
 $("#mousecolors > .orange").addClass("selected");
 
-
+//change background color
 $(".backgroundSquare").on("click", function(){
 	$(".backgroundSquare").removeClass("selected");
 	$(this).addClass("selected");
@@ -17,23 +19,25 @@ $(".backgroundSquare").on("click", function(){
 	$(".square").css("background", backgroundCol);
 });
 
+//change mouseover color
 $(".mouseSquare").on("click", function(){
 	$(".mouseSquare").removeClass("selected");
 	$(this).addClass("selected");
 	mouseCol=$(this).css("background-color");
 });
 
-
+//load number of squares and colors
 for(var i=0;i<totalBoxes;i++) {
 	$("#wrapper").append("<div class='square'> </div>");
 	$(".square").css("width", boxWidth);
 	$(".square").css("height", boxHeight);
 	$(".square").on("mouseenter", function() {
 		$(this).css("background", mouseCol);
-	})
+	});
 }
 
+//reset to selected background color
 $("#resetBtn").on("click", function(){
 	$(".square").css("background", backgroundCol);
-})
+});
 
